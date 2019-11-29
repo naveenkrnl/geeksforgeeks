fcf49488276d18003bfc77e9e8277f990165b46c
naveen <naveenarorakrnl@gmail.com>
2019-11-29 11:07:12 +0530

    filepathfield

--- geeks/forms.py
+++ geeks/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 
-# creating a form 
-class InputForm(forms.Form):
-    first_name   =   forms.CharField(max_length=200)
-    last_name    =   forms.CharField(max_length=200)
-    roll_number  =   forms.IntegerField(help_text="Enter 6 digit roll number")
-    password     =   forms.CharField(widget=forms.PasswordInput())
+
+class GeeksForm(forms.Form):
+    name         = forms.CharField()
+    geeks_field  = forms.FilePathField(path="geeksforgeeks/")
--- geeks/views.py
+++ geeks/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from .forms import InputForm
+from .forms import GeeksForm
 
+def handle_uploaded_file(f):  
+    with open('geeks/upload/'+f.name, 'wb+') as destination:  
+        for chunk in f.chunks():
+            destination.write(chunk)  
 # Create your views here.
 def home_view(request):
     context={}
-    context['form']=InputForm()
+    if request.POST:
+        form=GeeksForm(request.POST)
+        print(request.POST)
+    else:
+        form=GeeksForm()
+    context['form']=form
     return render(request,"home.html",context)
