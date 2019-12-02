4f00d2021574a91d84f7ac803c03f9b75a750010
naveen <naveenarorakrnl@gmail.com>
2019-12-02 21:39:18 +0530

    Django Forms SlugField

--- geeks/forms.py
+++ geeks/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 
-DEMO_CHOICES=(
-    ("1","Naveen"),
-    ("2","Pranav"),
-    ("3","Isha"),
-    ("4","Saloni"),
-)
+# DEMO_CHOICES=(
+#     ("1","Naveen"),
+#     ("2","Pranav"),
+#     ("3","Isha"),
+#     ("4","Saloni"),
+# )
 
 class GeeksForm(forms.Form):
-    geeks_field         = forms.SlugField()
+    geeks_field         = forms.SlugField()ls
+    
