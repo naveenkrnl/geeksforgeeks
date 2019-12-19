d846c01316afae515ca0d4adf1bbff0b4bafdbbd
Naveen Arora <naveenarorakrnl@gmail.com>
2019-12-19 18:32:08 +0530

    Django Models

--- geeks/models.py
+++ geeks/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.utils.text import slugify 
 
 # Create your models here.
 class GeeksModel(models.Model):
-	title=models.CharField(max_length=200)
-	img=models.ImageField(upload_to="images/")
+	title = models.CharField(max_length=200)
+	description = models.TextField()
+	last_modified = models.DateTimeField(auto_now_add=True)
+	img = models.ImageField(upload_to="images/")
 
 	def __str__(self):
 		return self.title
