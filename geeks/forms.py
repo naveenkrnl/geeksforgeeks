17ec998d7bf646bd15ce3225737897af176900c5
Naveen Arora <naveenarorakrnl@gmail.com>
2019-12-26 11:22:32 +0530

    Django Forms

--- geeks/forms.py
+++ geeks/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 
 class GeeksForm(forms.Form):
-    title         = forms.CharField(initial = "Method 2 ")
-    description   = forms.CharField(initial = "Method 2 description")
-    available     = forms.BooleanField(initial = True)
-    email         = forms.EmailField(initial = "abc@gmail.com")
-
-
+    title         =  forms.CharField()
+    description   =  forms.CharField()
+    views         =  forms.IntegerField()
+    date          =  forms.DateField(widget=forms.SelectDateWidget)
