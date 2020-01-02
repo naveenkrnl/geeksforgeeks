af6d99024e86796ca02b94f0a6a17cf4a9b01ef7
Naveen Arora <naveenarorakrnl@gmail.com>
2020-01-02 12:02:35 +0530

    trash

--- geeks/forms.py
+++ geeks/forms.py
@@ -1,12 +1,6 @@
-# import form class from django
 from django import forms
-
-# import GeeksModel from models.py
-from .models import GeeksModel
-
-# create a ModelForm
-class GeeksForm(forms.ModelForm):
-    # specify the name of model to use
-    class Meta:
-        model=GeeksModel
-        fields = "__all__"
+ 
+# creating a form 
+class InputForm(forms.Form):
+    first_name = forms.CharField(max_length = 200)
+    img = forms.FileField()
--- geeks/views.py
+++ geeks/views.py
@@ -1,47 +1,32 @@
-
 from django.shortcuts import render
-from .forms import GeeksForm
+from .forms import InputForm
+ 
+# Create your views here.
+# def home_view(request):
+#     context ={}
+#     context['form']= InputForm()
+#     return render(request, "home.html", context)
+
+
+
+
+
+
 
 def home_view(request):
     context ={}
 
-    # create object of form
-    form = GeeksForm(request.POST or None, request.FILES or None)
-    
-    # check if form data is valid
-    if form.is_valid():
-        # save the form data to model
-        form.save()
+    # dictionary for initial data with 
+    # field names as keys
 
+    # add the dictionary during initialization
+    form = InputForm(request.POST or None,request.FILES or None)
+    if form.is_valid():
+        print(type(request.FILES['img']))
+
+        
     context['form']= form
     return render(request, "home.html", context)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def home_view(request):
-#     context ={}
-
-#     # dictionary for initial data with 
-#     # field names as keys
-
-#     # add the dictionary during initialization
-#     form = GeeksForm(request.POST or None)
-
-#     context['form']= form
-#     return render(request, "home.html", context)
 
 
 # from django.shortcuts import render
