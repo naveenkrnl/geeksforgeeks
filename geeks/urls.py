a49bdd0c80edaf40fbfc08de01293168f488f57a
Naveen Arora <naveenarorakrnl@gmail.com>
2020-01-13 11:37:08 +0530

    Update View - Function Based Views Django

--- geeks/urls.py
+++ geeks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 # importing views from views..py
-from .views import update_view
+from .views import update_view ,detail_view
 
 urlpatterns = [
+    path('<id>/', detail_view ),
     path('<id>/update', update_view ),
 ]
--- geeks/views.py
+++ geeks/views.py
@@ -1,17 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render ,HttpResponseRedirect
 
 # relative import of forms
 from .models import GeeksModel
+from .forms import GeeksForm
 
-# pass id attribute from urls
+# after updating it will redirect to detail_View
+def detail_view(request,id):
+    # dictionary for initial data with 
+    # field names as keys
+    context ={}
+ 
+    # add the dictionary during initialization
+    context["data"] = GeeksModel.objects.get(id=id)
+         
+    return render(request, "detail_view.html", context)
+
+# update view for details
 def update_view(request,id):
     # dictionary for initial data with 
     # field names as keys
     context ={}
 
-    # add the dictionary during initialization
-    context["data"] = GeeksModel.objects.get(id=id)
-        
+    # fetch the object related to passed id
+    obj = get_object_or_404(GeeksModel, id=id)
+
+    # pass the object as instance in form
+    form = GeeksForm(request.POST or None, instance = obj)
+
+    # save the data from the form and
+    # redirect to detail_view
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/"+id)
+
+    # add form dictionary to context
+    context["form"] = form
+
     return render(request, "update_view.html", context)
 
 
