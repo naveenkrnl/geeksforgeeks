e3564e9f55b2234f762aafbe4de65c2b2066c618
Naveen Arora <naveenarorakrnl@gmail.com>
2020-01-15 12:54:41 +0530

    Function based views Django

--- geeks/urls.py
+++ geeks/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 # importing views from views..py
-from .views import update_view ,detail_view
-
+from .views import delete_view
 urlpatterns = [
-    path('<id>/', detail_view ),
-    path('<id>/update', update_view ),
+    path('<id>/delete', delete_view ),
 ]
--- geeks/views.py
+++ geeks/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import get_object_or_404,render ,HttpResponseRedirect
 
-# relative import of forms
 from .models import GeeksModel
-from .forms import GeeksForm
 
-# after updating it will redirect to detail_View
-def detail_view(request,id):
-    # dictionary for initial data with 
-    # field names as keys
-    context ={}
- 
-    # add the dictionary during initialization
-    context["data"] = GeeksModel.objects.get(id=id)
-         
-    return render(request, "detail_view.html", context)
 
-# update view for details
-def update_view(request,id):
+# delete view for details
+def delete_view(request,id):
     # dictionary for initial data with 
     # field names as keys
     context ={}
@@ -24,27 +12,12 @@ def update_view(request,id):
     # fetch the object related to passed id
     obj = get_object_or_404(GeeksModel, id=id)
 
-    # pass the object as instance in form
-    form = GeeksForm(request.POST or None, instance = obj)
 
-    # save the data from the form and
-    # redirect to detail_view
-    if form.is_valid():
-        form.save()
-        return HttpResponseRedirect("/"+id)
+    if request.method=="POST":
+        # delete object
+        obj.delete()
+        # after deleting redirect to 
+        # home page
+        return HttpResponseRedirect("/")
 
-    # add form dictionary to context
-    context["form"] = form
-
-    return render(request, "update_view.html", context)
-
-
-# from django.shortcuts import render
-# from .forms import GeeksForm
-
-# # creating a home view
-# def home_view(request):
-#     context = {}
-#     form = GeeksForm(request.POST or None)
-#     context['form'] = form
-#     return render(request, "home.html", context)
+    return render(request, "delete_view.html", context)
