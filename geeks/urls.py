6f2308b64dbfa20c98e977667014b7ae61a0b351
naveenkrnl <naveenarorakrnl@gmail.com>
2020-02-05 11:38:36 +0530

    url - Django Template Tags

--- geeks/urls.py
+++ geeks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 # importing views from views..py
-from .views import geeks_view
+from .views import geeks_view , nav_view
 
 urlpatterns = [
-    path('', geeks_view),
+    path('1/', geeks_view, name = "template1"),
+    path('2/', nav_view,name = "template2"),
 ]
--- geeks/views.py
+++ geeks/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
  
 # create a function
 def geeks_view(request):
-    # create a dictionary
-    context = {
-        "data" : False,
-    }
     # return response
-    return render(request,"geeks.html",context)
+    return render(request,"geeks.html")
+
+def nav_view(request):
+    # return response
+    return render(request,"nav.html")
